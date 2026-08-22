228c90cd7e0a0f43b18de2e98b71f3f99d0b929e
Belo <guihbelofontes@gmail.com>
2026-08-22 19:54:51 -0300

    Implementa reconstrução da solucao do SAT resolver

--- decodificador.py
+++ decodificador.py
@@ -1,3 +1,5 @@
+import argparse
+
 def decodificar_variavel(identificador: int, n: int) -> tuple[int,int,int]:
     identificador -=1 
 
@@ -9,8 +11,50 @@ def decodificar_variavel(identificador: int, n: int) -> tuple[int,int,int]:
 
     return i+1, j+1, k+1
 
+def ler_modelo(caminho: str) -> list[int]:
+    literais = []
+
+    with open(caminho, "r", encoding="utf-8") as arquivo:
+        for linha in arquivo:
+            if linha.startswith("v "):
+                valores = linha.split()[1:]
+
+                for valor in valores:
+                    literal = int(valor)
+
+                    if literal != 0: 
+                        literais.append(literal)
+    return literais   
+
+def reconstruir_grade(literais: list[int], n: int) -> list[list[int]]:
+    grade = [[0 for _ in range(n)] for _ in range(n)]
+
+    for literal in literais:
+        if literal > 0:
+            linha, coluna, valor = decodificar_variavel(literal, n)
+
+            grade[linha - 1][coluna - 1] = valor
+    return grade
+
+def exibir_grade(grade: list[list[int]]) -> None: 
+    for linha in grade:
+        print(" ".join(map(str, linha)))
+
+def main() -> None:
+
+    parser = argparse.ArgumentParser(description="Reoconstrói a solução de um Chaos sudoku a partir do modelo do Cadical")
+
+    parser.add_argument("resultado", help ="Arquivo contendo a saida do Cadical")
+
+    parser.add_argument("n", type=int, help="Tamanho N da grade")
+
+    argumentos = parser.parse_args()
+
+    literais = ler_modelo(argumentos.resultado)
+
+    grade = reconstruir_grade(literais, argumentos.n)
+
+    exibir_grade(grade)
+
 if __name__ == "__main__":
-    print(decodificar_variavel(1,4))
-    print(decodificar_variavel(5,4))
-    print(decodificar_variavel(28,4))
-    print(decodificar_variavel(64,4))
+    main()
