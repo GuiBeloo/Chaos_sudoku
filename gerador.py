685ad02f53d9d8147700cd4308fa37347442cd48
Belo <guihbelofontes@gmail.com>
2026-08-22 12:44:59 -0300

    Implementa validação das instancias

--- gerador.py
+++ gerador.py
@@ -3,7 +3,8 @@ from typing import List, Tuple
 Celula = tuple[int, int]
 Clausula = List[int]
 Regioes = List[List[Celula]]
-
+Pista = Tuple[int, int, int]
+Pistas = List[Pista]
 
 def id_variavel(i: int, j: int, k: int, n:int) -> int:
     return (i-1) * n**2 + (j-1) * n + k
@@ -38,12 +39,40 @@ def ler_instancia(caminho: str):
 
         for indice in range(n+2, n+2+quantidade_pistas):
             linhas, coluna, valor = map(int, linhas[indice].split())      
-            pistas.append((linha,coluna,valor))
+            pistas.append(( linha, coluna, valor))
 
     return n, regioes, pistas        
 
+def validar_instancia(n: int, regioes: Regioes, pistas: Pistas) -> None:
+    if n <= 0:
+        raise ValueError("O tamanho N deve ser maior que zero.")
+    if len(regioes) != n:
+        raise ValueError(f"A instancia deve possuir exatamente {n} regiões")
+
+    celulas_encontradas = set()
+
+    for indice,regiao in enumerate(regioes, start=1):
+        if len(regiao) != n:
+            raise ValueError(f"A região {indice} deve possuir exatamente {n} células")
+
+        for linha, coluna in regiao:
+            if not(1 <= linha <= n and 1<= coluna <= n):
+                raise ValueError(f"Célula ({linha}, {coluna}) fora dos limites da grade")
+            if(linha, coluna) in celulas_encontradas:
+                raise ValueError(f"A célula ({linha}, {coluna}) aparece em mais de uma região.")
+            celulas_encontradas.add((linha,coluna))
+
+    if len(celulas_encontradas) != n**2:
+        raise ValueError("Nem todas as células da grade pertencem a uma região")
+
+    for linha, coluna, valor in pistas:
+        if not(1 <= linha <= n and 1 <= coluna <= n and 1 <= valor <= n):
+            raise ValueError(f"Pista inválida: ({linha}, {coluna}, {valor})")
+
 if __name__ == "__main__":
     n,regioes, pistas = ler_instancia("instancias/chaos4_01.txt")
+
+    validar_instancia(n, regioes, pistas)
 
     print(f"N: {n}")
     print(f"Regiões: {regioes}")
