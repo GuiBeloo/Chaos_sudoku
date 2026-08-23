f809effb6f6990e89be9b68fa700b323e38e0a7c
Belo <guihbelofontes@gmail.com>
2026-08-22 23:10:40 -0300

    Integra decodificador com arquivo de instancia

--- decodificador.py
+++ decodificador.py
@@ -40,21 +40,49 @@ def exibir_grade(grade: list[list[int]]) -> None:
     for linha in grade:
         print(" ".join(map(str, linha)))
 
+def ler_instancia(caminho: str) -> tuple[int, list[list[str]]]:
+
+    with open(caminho, "r", encoding="utf-8") as arquivo:
+        linhas = [linha.strip() for linha in arquivo if linha.strip()]
+
+    n = int(linhas[0])
+    grade_regioes = []
+
+    for i in range(1, n+1):
+        grade_regioes.append(linhas[i].split())
+    return n, grade_regioes
+
+def exibir_regioes(grade_regioes: list[list[str]]) -> None:
+    print("\nRegiões:")
+
+    for linha in grade_regioes:
+        print(" ".join(linha))
+
+def exibir_grade(grade: list[list[int]]) -> None:
+    print("Solução:")
+
+    for linha in grade:
+        print(" ".join(map(str,linha)))
+
 def main() -> None:
 
     parser = argparse.ArgumentParser(description="Reoconstrói a solução de um Chaos sudoku a partir do modelo do Cadical")
 
-    parser.add_argument("resultado", help ="Arquivo contendo a saida do Cadical")
+    parser.add_argument("resultado", help ="Arquivo contendo a saída do CadicaL")
 
-    parser.add_argument("n", type=int, help="Tamanho N da grade")
+    parser.add_argument("instancia",help="Arquivo contendo a instância do Chaos Sudoku")
 
     argumentos = parser.parse_args()
 
+    n, grade_regioes = ler_instancia(argumentos.instancia)
+
     literais = ler_modelo(argumentos.resultado)
 
-    grade = reconstruir_grade(literais, argumentos.n)
+    grade = reconstruir_grade(literais, n)
 
     exibir_grade(grade)
+    exibir_regioes(grade_regioes)
+
 
 if __name__ == "__main__":
     main()
